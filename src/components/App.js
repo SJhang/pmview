07ca022a1210b44d9ea305c63844f8b09bb867b7
Jimmy Hong <jimmyhong.rocks@gmail.com>
2018-06-01 21:17:45 -0500

    create mock api for fetching

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import Home from "./Home";
 import StoreInfo from "./StoreInfo";
 import StoreReview from "./StoreReview";
 // custom components
+import Test from './Test';
 
 const Root = () => {
   return (
     <BrowserRouter>
       <React.Fragment>
         <Header />
+        <Test />
         <Switch>
           <Route component={Home} exact path="/" />
           <Route component={StoreInfo} path="/store-info" />
